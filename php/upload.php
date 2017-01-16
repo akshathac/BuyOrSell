bf8244c020bb3befb75c3ae7aa77731dbe0980aa
Akshatha Channakrishnappa <akshathac91@gmail.com>
2017-01-15 23:28:09 -0600

    Update upload.php
    Removed credentials

--- php/upload.php
+++ php/upload.php
@@ -9,11 +9,10 @@ require 'C:/Users/akshatha/PhpstormProjects/BuyOrSell/aws/aws-autoloader.php';
 $file_name = $_FILES["file_upload"]["name"];
 $ext = pathinfo($file_name, PATHINFO_EXTENSION);
 $new_file_name = md5(uniqid(rand(), true)).".".$ext;
-$bucket = "shoppingimagedir";
 
 use Aws\S3\S3Client;
 
-$bucket = 'shoppingimagedir';
+$bucket = //Bucket name;
 $keyname = $file_name;
 // $filepath should be absolute path to a file on disk
 $filepath = $_FILES["file_upload"]["tmp_name"];
@@ -24,8 +23,8 @@ $s3 = S3Client::factory(array(
     'region'  => 'us-east-1',
     'version' => 'latest',
     'credentials' => [
-    'key'    => 'AKIAJAO4YKJ65YXM3HPA',
-    'secret' => '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',
+    'key'    => //AWS Key,
+    'secret' => //AWS Secret key,
 ],
         'http'    => [
             'verify' => 'C:\Users\akshatha\PhpstormProjects\BuyOrSell\cacert.pem'
